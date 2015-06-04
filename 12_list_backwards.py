6ae5e68818d11a06d6ba27fdc8e9ed6e7bf7a7a9
Brian Crites <brrcrites@gmail.com>
2015-06-03 17:40:10 -0700

    Added first Law of Gravity example and updated backwards list example for readability

--- 12_list_backwards.py
+++ 12_list_backwards.py
@@ -1,6 +1,6 @@
 sequence = [4,8,15,16,23,42]
-index = -1
-while index + len(sequence) >= 0:
-	print sequence[index]
-	index = index - 1
+index = 0
+while len(sequence) - index >= 0:
+	print sequence[len(sequence) - index]
+	index = index + 1
 	
