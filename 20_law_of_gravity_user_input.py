894f1dd65f764c85ac40840e28a503e7474d4636
Brian Crites <brrcrites@gmail.com>
2015-07-08 08:48:23 +0000

    Updated 0 and 20 gravity examples so they have better naming conventions

--- 20_law_of_gravity_user_input.py
+++ 20_law_of_gravity_user_input.py
@@ -1,14 +1,14 @@
 #Get the mass of the two planets, and the distance between the planets
-mass1 = raw_input("Enter first planet's mass: ")
-mass2 = raw_input("Enter second planet's mass: ")
-distance = raw_input("Enter the distance between the planets: ")
+mass1_input = raw_input("Enter first planet's mass: ")
+mass2_input = raw_input("Enter second planet's mass: ")
+distance_input = raw_input("Enter the distance between the planets: ")
 
 #Convert the user input into integer numbers
-m1 = int(mass1)
-m2 = int(mass2)
-d = int(distance)
+mass1 = int(mass1_input)
+mass2 = int(mass2_input)
+distance = int(distance_input)
 
 #Set the gravity constant, solve the equation, and print
-G = 6.673*(10**(-11))
-F = (G*m1*m2)/d**2
-print F
+gravity = 6.673*(10**(-11))
+force = (gravity*mass1*mass2)/distance**2
+print force
