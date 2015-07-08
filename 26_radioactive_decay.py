ea5f44efbb637c4c1c8dd20b4f2ef63e115e3196
Brian Crites <brrcrites@gmail.com>
2015-07-08 07:22:20 +0000

    Made corrections to the radioactive decay examples

--- 26_radioactive_decay.py
+++ 26_radioactive_decay.py
@@ -1,10 +1,10 @@
-t = 0       #Set time to 0
-e = 2.72    #Eulers constant
+current_time = 0        #Set the current time to 0 (because we're just starting)
+e = 2.72                #Eulers constant
 
 decay_constant = float(raw_input("decay constant: "))
-amount = float(raw_input("amount of radioactive material: "))
+start_amount = float(raw_input("amount of radioactive material: "))
 
-while t < 10:
-    amount = amount * (e ** (-1 * decay_constant * t))
+while current_time < 10:
+    amount = start_amount * (e ** (-1 * decay_constant * current_time))
     print amount
-    t = t + 1
+    current_time = current_time + 1
