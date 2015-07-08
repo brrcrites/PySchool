e9e0474b255ccaeb9e06371aa2db928af2829621
Brian Crites <brrcrites@gmail.com>
2015-07-08 07:32:32 +0000

    Added number validation for radioactive decay example

--- 27_radioactive_decay_2.py
+++ 27_radioactive_decay_2.py
@@ -5,6 +5,10 @@ decay_constant = float(raw_input("decay constant: "))
 start_amount = float(raw_input("amount of radioactive material: "))
 stop_time = int(raw_input("amount of time to decay: "))
 
+if stop_time < 0 or stop_time > 1000:
+    print "Please enter an amount of time between 1 and 999"
+    exit()
+
 while current_time < stop_time:
     amount = start_amount * (e ** (-1 * decay_constant * current_time))
     print amount
