1d8b5a7feaeea7116f54980087b2753d8aa4a796
Brian Crites <brrcrites@gmail.com>
2015-07-10 06:34:02 +0000

    Updated 1B to match changes in slides

--- 1B_if_else.py
+++ 1B_if_else.py
@@ -1,11 +1,12 @@
 x = 2
 y = 1
 if y < x:
-    print “y < x”
+    print "y < x"
     y = y + 10
 if x < y:
-    print “x < y”
+    print "x < y"
     x = x + 15
-print x
+if x > 10:
+    print x
 if y > 10:
     print y
