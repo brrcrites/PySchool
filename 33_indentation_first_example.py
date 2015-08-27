8e068ed03172639b6ebcf8da39b6d2fc684fe1cb
Brian Crites <brrcrites@gmail.com>
2015-08-27 22:59:00 +0000

    indentation example added

---
+++ 33_indentation_first_example.py
@@ -0,0 +1,10 @@
+i = 2
+j = 1
+print "unindented code will always print"
+if i == 1:
+    print "but this only prints if i is 1"
+if i == 2:
+    print "and this only prints if i is 2"
+    if j == 1:
+        print "this prints when i is 2 and j is 1"
+  
